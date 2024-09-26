0db2ac3f14b16afae319b5b1648cc43d07d9c7e0
bakemono-san <mousdef34@gmail.com>
2024-09-26 10:11:32 +0000

    ahahaah

--- =/app/Services/Contracts/FileStorageServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-// /app/Services/Contracts/FileStorageServiceInterface.php
-namespace App\Services\Contracts;
-
-interface FileStorageServiceInterface
-{
-    public function store($file, $path);
-    public function getBase64($path);
-}
